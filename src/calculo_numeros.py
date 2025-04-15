047c559a7eae26d5a8bb09f39a2a3cdd5b18bde1
Juan Pablo Carini <j.carini@alumno.um.edu.ar>
2025-04-15 19:18:00 -0300

    funcion

--- src/calculo_numeros.py
+++ src/calculo_numeros.py
@@ -1,5 +1,5 @@
+'''
 from exceptions import ingrese_numero
-from exceptions import NumeroDebeSerPositivo
 
 def main():
     """
@@ -19,3 +19,4 @@ def main():
 
 if __name__ == "__main__":
     main() 
+ '''
